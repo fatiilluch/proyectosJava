896dd9e66c5d204d1bd74763f460646f6b4353b0
Fátima Lluch <32984924+fatiilluch@users.noreply.github.com>
2019-02-12 17:45:25 -0300

    cambio controller
    Ahora se puede ingresar tantos usuarios al archivo json como se desee

--- TrelloFile/src/main/java/com/ar/cda/controller/TrelloController.java
+++ TrelloFile/src/main/java/com/ar/cda/controller/TrelloController.java
@@ -1,83 +1,116 @@
 package com.ar.cda.controller;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Scanner;
+
 import javax.servlet.http.HttpServletRequest;
 
-import org.json.simple.*;
-
+import org.json.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.ar.cda.model.Trello;
 
 @Controller
 public class TrelloController {
 
-//    /**
-//     * @param response
-//     * @param file
-//     * @throws Exception
-//     */
-//    @RequestMapping(value = "downloadFile", method = RequestMethod.GET)
-//    @ResponseBody
-//    public void descargarArchivo(HttpServletResponse response, String file) throws Exception
-//    {	
-//      TrelloService trelloService = new TrelloService();
-//      byte[] archivo = trelloService.leerArchivo(file).getBytes();
-//      response.setContentType("application/octet-stream"); 
-//      response.setHeader("Content-Disposition", "filename = ArchivoTrello.json"); 
-//      trelloService.leerArchivoJSON(file);
-//      OutputStream outputStream = response.getOutputStream();
-//      outputStream.write(archivo);
-//      outputStream.close();
-//      }
-    
-//	//ver pagina www.uv.es/grimo/teaching/springMVC4pasoapaso/
+	// /**
+	// * @param response
+	// * @param file
+	// * @throws Exception
+	// */
+	// @RequestMapping(value = "downloadFile", method = RequestMethod.GET)
+	// @ResponseBody
+	// public void descargarArchivo(HttpServletResponse response, String file)
+	// throws Exception
+	// {
+	// TrelloService trelloService = new TrelloService();
+	// byte[] archivo = trelloService.leerArchivo(file).getBytes();
+	// response.setContentType("application/octet-stream");
+	// response.setHeader("Content-Disposition", "filename =
+	// ArchivoTrello.json");
+	// trelloService.leerArchivoJSON(file);
+	// OutputStream outputStream = response.getOutputStream();
+	// outputStream.write(archivo);
+	// outputStream.close();
+	// }
 
+	// //ver pagina www.uv.es/grimo/teaching/springMVC4pasoapaso/
+
+	@SuppressWarnings({ "unchecked" })
   @RequestMapping(value = "recibeDatos", method = RequestMethod.GET)
-  
-  public String trello(HttpServletRequest req) 
+  public String trello(HttpServletRequest req) throws ParseException 
   {	
-	JSONObject objeto = new JSONObject();
+	File file = new File("C:\\workspace2\\pruebaDos.json");
+	Object object = new Object();
 	JSONObject datos = new JSONObject();
+	JSONArray array = new JSONArray();
 	
-	try {
-		
-		datos.put("nombre",req.getParameter("nombre"));
-		datos.put("apellido", req.getParameter("apellido"));
-		datos.put("edad", req.getParameter(String.valueOf("edad")));
-		objeto.put("datos", datos);
-		
-		
-		FileWriter writer = new FileWriter("C:\\Users\\L0649805\\workspace2\\pruebaDos.json");
-		writer.write(objeto.toJSONString());
-		writer.flush();
-		writer.close();
-	} catch (IOException e) {
-		e.printStackTrace();
+	if(file.exists() && file.length()!=0)
+	{
+		try
+		{
+			FileReader fr = new FileReader(file); // lee el archivo 
+			JSONParser parser = new JSONParser(); //creo el parser
+			object = parser.parse(fr); //guardamos en el objeto lo que se va a leer
+			array.put(object); //ponemos el archivo en un array
+			
+			datos.put("nombre",req.getParameter("nombre"));
+			datos.put("apellido", req.getParameter("apellido"));
+			datos.put("edad", req.getParameter(String.valueOf("edad")));
+				
+			array.put(datos);
+				
+			FileWriter writer = new FileWriter("C:\\workspace2\\pruebaDos.json");
+			BufferedWriter bw = new BufferedWriter(writer);
+			bw.write(array.toString());
+			bw.newLine();
+			bw.flush();
+			bw.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	else
+	{
+		try
+		{
+			datos.put("nombre",req.getParameter("nombre"));
+			datos.put("apellido", req.getParameter("apellido"));
+			datos.put("edad", req.getParameter(String.valueOf("edad")));
+				
+			array.put(datos);
+				
+			FileWriter writer = new FileWriter("C:\\workspace2\\pruebaDos.json");
+			BufferedWriter bw = new BufferedWriter(writer);
+			bw.write(array.toString());
+			bw.newLine();
+			bw.flush();
+			bw.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	return "redirect:trello.jsp";
   }
-	String asd =  objeto.getJSONObject("pageInfo").getString("pageName");
-
 	
-	System.out.println(objeto.get("datos").toString());
-	return "redirect:\ttrello.jsp";
- }
-	
-//	//@ResponseBody
-//    @RequestMapping(value = "recibeDatos",params = {"nombre", "apellido", "edad" }, method = RequestMethod.GET)
-//    public String trello(Model model, 
-//    		 @RequestParam("nombre") String nombre,
-//    	     @RequestParam("apellido") String apellido,
-//    	     @RequestParam("edad") int edad) 
-//    {
-//    	Trello persona = new Trello (nombre, apellido, edad);
-//    	
-//    	model.addAttribute("persona",persona);
-//   
+}
+		
 //    	return "recibeDatos";
 //    }
 	
